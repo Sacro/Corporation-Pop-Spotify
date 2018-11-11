2ed0a951d88e451c8b5c0bc2b1a5920b3c0a1f28
Ben Woodward <ben@benwoodward.me.uk>
2018-11-11 20:55:17 +0000

    Working artists!

--- frontend/src/app/artists/artists.module.ts
+++ frontend/src/app/artists/artists.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
+import { ArtistItemComponent } from './artist-item/artist-item.component';
 import { ArtistsRoutingModule } from './artists-routing.module';
 import { ArtistsComponent } from './artists/artists.component';
 
 @NgModule({
-  declarations: [ArtistsComponent],
-  imports: [
-    CommonModule,
-    ArtistsRoutingModule
-  ]
+  declarations: [ArtistsComponent, ArtistItemComponent],
+  imports: [CommonModule, ArtistsRoutingModule, NgZorroAntdModule]
 })
-export class ArtistsModule { }
+export class ArtistsModule {}
--- frontend/src/app/artists/artists/artists.component.ts
+++ frontend/src/app/artists/artists/artists.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from 'src/app/search/search.service';
 
 @Component({
   selector: 'app-artists',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artists.component.css']
 })
 export class ArtistsComponent implements OnInit {
+  protected searchResults;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private searchService: SearchService) {
+    this.searchService.searchResults.subscribe(value => {
+      this.searchResults = value;
+      console.log(value);
+    });
   }
 
+  ngOnInit() {}
 }
