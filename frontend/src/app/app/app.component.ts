7b11915020377a3773b56df660db5fc0e50392b9
Ben Woodward <ben@benwoodward.me.uk>
2018-11-11 16:16:07 +0000

    Basic homepage layout

--- frontend/src/app/app/app.component.ts
+++ frontend/src/app/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend';
+  isLoading: boolean;
+  search: string;
+
+  onSearchClicked() {
+    if (this.search) {
+      console.log(this.search);
+    }
+  }
 }
